e784c732ab156f37af7fcc24cd0bfce0c1b123e3
crme291 <deeavinipu8@gmail.com>
2026-01-16 18:37:19 +0200

    add model

---
+++ src/main/java/com/example/application/model/Task.java
@@ -0,0 +1,14 @@
+package com.example.application.model;
+
+import java.time.LocalDate;
+
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate creationDate;
+    private Boolean completed;
+
+
+}
---
+++ src/main/java/com/example/application/model/User.java
@@ -0,0 +1,13 @@
+package com.example.application.model;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+//    private List<>
+}
