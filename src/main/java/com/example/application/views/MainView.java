a48ca93af9b921befc46be4b03aae15077668865
crme291 <deeavinipu8@gmail.com>
2026-01-16 18:23:57 +0200

    main page

--- src/main/java/com/example/application/views/MainView.java
+++ src/main/java/com/example/application/views/MainView.java
@@ -4,57 +4,46 @@ import com.example.application.service.GreetService;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-/**
- * A sample Vaadin view class.
- * <p>
- * To implement a Vaadin view just extend any Vaadin component and use @Route
- * annotation to announce it in a URL as a Spring managed bean.
- * <p>
- * A new instance of this class is created for every new user and every browser
- * tab/window.
- * <p>
- * The main view contains a text field for getting the user name and a button
- * that shows a greeting message in a notification.
- */
 @Route
 public class MainView extends VerticalLayout {
 
-    /**
-     * Construct a new Vaadin view.
-     * <p>
-     * Build the initial UI state for the user accessing the application.
-     *
-     * @param service
-     *            The message service. Automatically injected Spring managed bean.
-     */
-    public MainView(GreetService service) {
+  public MainView(GreetService service) {
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addClassName("bordered");
+        Paragraph paragraph = new Paragraph("TODO app");
+        paragraph.addClassName("main-view-paragraph");
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello", e -> {
-            add(new Paragraph(service.greet(textField.getValue())));
-        });
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
+//       change button color
+        Button button = new Button("Sign in");
+        button.addClassName("sign-in-button");
+        button.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
         button.addClickShortcut(Key.ENTER);
+
+        Div div = getDiv(button);
 
         // Use custom CSS classes to apply styling. This is defined in
         // styles.css.
         addClassName("centered-content");
 
-        add(textField, button);
+        add(paragraph, div);
     }
+
+    private static Div getDiv(Button button) {
+        TextField usernameTextField = new TextField("Username");
+        usernameTextField.addClassName("username-input-field");
+
+        TextField passwordTextField = new TextField("Password");
+        passwordTextField.addClassName("password-input-field");
+
+        Div div = new Div(usernameTextField, passwordTextField, button);
+        div.addClassName("sign-in-view-div");
+
+        return div;
+    }
+
 }
